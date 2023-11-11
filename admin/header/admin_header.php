3838964e4b4e3e4a55b7bbf0e55ee0df13e70a91
Vignesh <vigneshpamu2002@gmail.com>
2023-11-11 21:12:50 +0530

    first-commit

---
+++ admin/header/admin_header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<?php 
+require_once($_SERVER['DOCUMENT_ROOT'].'/includes/init.php');
+?>
+<html lang="en">
+<head><title>Automated Test Paper Generator | Home</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- STYLE CSS    -->
+	<link type="text/css" rel="stylesheet" href="/assets/css/custom.css">
+	<!--Jquery  
+	<script src="../../assets/libs/jquery/jquery-2.1.4.min.js"></script>   -->
+	
+</head>
+<body>
+	<header>
+		<!-- Navbar -->
+		<div class="navbar-top">
+			<div class="nav-bar nav-center navbar-black">
+				<a href="/admin/index.php" class="navbar-item navbar-button navbar-padding-large">HOME</a>
+				<a href="/admin/stream/stream.php" class="navbar-item navbar-button navbar-padding-large">Stream</a>
+				<a href="/admin/departments/departments.php" class="navbar-item navbar-button navbar-padding-large">Departments</a>
+				<a href="/admin/degree/degree.php" class="navbar-item navbar-button navbar-padding-large">Degree</a>
+				<a href="/admin/class/class.php" class="navbar-item navbar-button navbar-padding-large">Class</a>
+				<a href="/admin/semester/semester.php" class="navbar-item navbar-button navbar-padding-large">Semester</a>
+				<a href="/admin/subject/subject.php" class="navbar-item navbar-button navbar-padding-large">Subject</a>
+				<a href="/admin/units/units.php" class="navbar-item navbar-button navbar-padding-large">Units</a>
+				<a href="/admin/questions/questions.php" class="navbar-item navbar-button navbar-padding-large">Questions</a>
+				<a href="/index.php" class="navbar-item-right navbar-button-right navbar-padding-large navbar-right">Log Out</a>
+			</div>
+		</div>
+	</header>
+</body>
+<script>
+</script>
+</html>
